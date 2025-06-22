94b455137cfe13a375cbd596d74f17ccc5fd94d7
Rksuryawanshi <rahulkumarkushwaha81@gmail.com>
2025-06-22 09:35:49 +0530

    palindrome code add

---
+++ Basics/palindrime.java
@@ -0,0 +1,27 @@
+package Basics;
+import java.util.Scanner;
+
+public class palindrime {
+    static boolean isPalindrome(int num){
+        int temp=num,newNum=0;
+        if(temp==0) return false;
+        while(temp!=0){
+            newNum=newNum*10+(temp%10);
+            temp/=10;
+        }
+        return newNum==num;
+    }
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter karo number");
+        int n=in.nextInt();
+
+        boolean x = isPalindrome(n);
+        if (x) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Ahh Oh na na na");
+        }
+        in.close();
+    }    
+}
